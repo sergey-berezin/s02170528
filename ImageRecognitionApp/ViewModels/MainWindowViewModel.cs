2a702e4d1919e3e6b45423ceb962be0d11242095
Maxim Kurkin <maximbider@gmail.com>
2020-10-27 14:49:52 +0300

    "Hello World!" Avalonia template.

---
+++ ImageRecognitionApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageRecognitionApp.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+    }
+}
